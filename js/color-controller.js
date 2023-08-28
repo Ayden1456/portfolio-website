eb3825b6548c0a976d21a7049e0e13f5964f04d1
Ayden1456 <89323066+Ayden1456@users.noreply.github.com>
2023-08-28 14:33:22 -0700

    add dark/light button

---
+++ js/color-controller.js
@@ -0,0 +1,25 @@
+let colorSwitchingButton = document.getElementById('color-switch');
+let root = document.querySelector(':root');
+let color = "light";
+
+const loadColors = (colors) => {
+    for (const property of colors) {
+        root.style.setProperty(...property);
+    }
+}
+
+const changeColor = () => {
+    if (color === "light") color = "dark";
+    else if (color === "dark") color = "light";
+    loadColors(colors[color]);
+}
+
+const changeIcon = () => {
+    colorSwitchingButton.classList.remove(...colorSwitchingButton.classList);
+    colorSwitchingButton.classList.add(color);
+}
+
+loadColors(colors['light']);
+changeIcon();
+colorSwitchingButton.addEventListener('click', () => { changeColor(); changeIcon(); });
+
---
+++ js/const.js
@@ -0,0 +1,22 @@
+let colors = {
+    'light': [
+        ['--mainColor', '#e7f3ff'],
+        ['--secondaryColor', '#cfe8ff'],
+        ['--borderColor', '#46d1e6'],
+        ['--mainText', '#0a0a0a'],
+        ['--secondaryText', '#414141'],
+        ['--themeDotBorder', '#050303'],
+        ['--previewBg', 'rgb(25, 39, 52, 0.8)'],
+        ['--previewShadow', '#e5e5e5'],
+    ],
+    'dark': [
+        ['--mainColor', '#15202b'],
+        ['--secondaryColor', '#192734'],
+        ['--borderColor', '#164D56'],
+        ['--mainText', '#fff'],
+        ['--secondaryText', '#eeeeee'],
+        ['--themeDotBorder', '#fff'],
+        ['--previewBg', 'rgb(25, 39, 52, 0.8)'],
+        ['--previewShadow', '#111921'],
+    ]
+}
